134daef85c49798577dbd8c5deeeeabe5caf7045
Bob Wei <bob.wei@linecorp.com>
2019-07-14 17:31:51 +0800

    Add audio recording permission to android

--- packages/app/index.js
+++ packages/app/index.js
@@ -2,8 +2,19 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform, PermissionsAndroid } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+if (Platform.OS === 'android') {
+  (async () => {
+    await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO, {
+      title: 'Record Audio Permission',
+      message: 'Record Audio Permission',
+      buttonNegative: 'Cancel',
+      buttonPositive: 'OK',
+    });
+  })();
+}
 
 AppRegistry.registerComponent(appName, () => App);
