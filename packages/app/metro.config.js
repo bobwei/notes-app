9d22c4c1d05c119ba1735f1be7a02a07439f7173
Bob Wei <bob.wei@linecorp.com>
2019-07-15 18:16:07 +0800

    Update metro.config.js to support yarn workspace usage

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -1,17 +1,27 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+
+const getWorkspaces = require('get-yarn-workspaces');
+const blacklist = require('metro-config/src/defaults/blacklist');
+
+const workspaces = getWorkspaces(__dirname);
+
+// Blacklists any react-native that doesn't come from packages/app
+const blacklistRE = blacklist([
+  /(?<!packages\/app\/)node_modules\/react-native\/.*/g,
+  /(?<!packages\/app\/)node_modules\/react\/.*/g,
+]);
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+  projectRoot: path.resolve(__dirname, '.'),
+
+  watchFolders: [path.resolve(__dirname, '../../node_modules'), ...workspaces],
+
+  resolver: {
+    blacklistRE,
+    extraNodeModules: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
   },
 };
