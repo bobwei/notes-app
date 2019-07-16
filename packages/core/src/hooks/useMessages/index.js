72c91f48a247b13e7482147ffda9485f30852d56
Bob Wei <bob.wei@linecorp.com>
2019-07-17 01:31:51 +0800

    Rm unnecessary

--- packages/core/src/hooks/useMessages/index.js
+++ packages/core/src/hooks/useMessages/index.js
@@ -18,38 +18,19 @@ const fn = ({ firebase, noteId, onNewMessage }) => {
         }
       });
 
-    db.collection('notes')
-      .doc(noteId)
-      .collection('messages')
-      .orderBy('createdAt', 'asc')
-      .get()
-      .then((snapshot) => {
-        const data = snapshot.docs.map((obj) => {
-          const { id } = obj;
-          return { id, ...obj.data() };
-        });
-        setMessages(data);
-      });
-
     const unsubscribe = db
       .collection('notes')
       .doc(noteId)
       .collection('messages')
       .orderBy('createdAt', 'asc')
-      .where('createdAt', '>', new Date())
       .onSnapshot((snapshot) => {
-        if (snapshot.docs.length) {
-          const docChanges = typeof snapshot.docChanges === 'function' ? snapshot.docChanges() : snapshot.docChanges;
-          const data = docChanges
-            .filter((change) => change.type === 'added')
-            .map((change) => {
-              const { id } = change.doc;
-              return { id, ...change.doc.data() };
-            });
-          setMessages((val) => [...val, ...data]);
-          if (onNewMessage) {
-            onNewMessage();
-          }
+        const data = snapshot.docs.map((doc) => {
+          const { id } = doc;
+          return { id, ...doc.data() };
+        });
+        setMessages(data);
+        if (onNewMessage) {
+          onNewMessage();
         }
       });
     return () => {
