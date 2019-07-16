e112e7173628912d0226239a0af3c1585fe9d295
Bob Wei <bob.wei@linecorp.com>
2019-07-17 01:16:00 +0800

    No need to check noteId

--- packages/core/src/hooks/useMessages/index.js
+++ packages/core/src/hooks/useMessages/index.js
@@ -4,47 +4,45 @@ const fn = ({ firebase, noteId, onNewMessage }) => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    if (noteId) {
-      const db = firebase.firestore();
+    const db = firebase.firestore();
 
-      db.collection('notes')
-        .doc(noteId)
-        .collection('messages')
-        .orderBy('createdAt', 'asc')
-        .get()
-        .then((snapshot) => {
-          const data = snapshot.docs.map((obj) => {
-            const { id } = obj;
-            return { id, ...obj.data() };
-          });
-          setMessages(data);
+    db.collection('notes')
+      .doc(noteId)
+      .collection('messages')
+      .orderBy('createdAt', 'asc')
+      .get()
+      .then((snapshot) => {
+        const data = snapshot.docs.map((obj) => {
+          const { id } = obj;
+          return { id, ...obj.data() };
         });
+        setMessages(data);
+      });
 
-      const unsubscribe = db
-        .collection('notes')
-        .doc(noteId)
-        .collection('messages')
-        .orderBy('createdAt', 'asc')
-        .where('createdAt', '>', new Date())
-        .onSnapshot((snapshot) => {
-          if (snapshot.docs.length) {
-            const docChanges = typeof snapshot.docChanges === 'function' ? snapshot.docChanges() : snapshot.docChanges;
-            const data = docChanges
-              .filter((change) => change.type === 'added')
-              .map((change) => {
-                const { id } = change.doc;
-                return { id, ...change.doc.data() };
-              });
-            setMessages((val) => [...val, ...data]);
-            if (onNewMessage) {
-              onNewMessage();
-            }
+    const unsubscribe = db
+      .collection('notes')
+      .doc(noteId)
+      .collection('messages')
+      .orderBy('createdAt', 'asc')
+      .where('createdAt', '>', new Date())
+      .onSnapshot((snapshot) => {
+        if (snapshot.docs.length) {
+          const docChanges = typeof snapshot.docChanges === 'function' ? snapshot.docChanges() : snapshot.docChanges;
+          const data = docChanges
+            .filter((change) => change.type === 'added')
+            .map((change) => {
+              const { id } = change.doc;
+              return { id, ...change.doc.data() };
+            });
+          setMessages((val) => [...val, ...data]);
+          if (onNewMessage) {
+            onNewMessage();
           }
-        });
-      return () => {
-        unsubscribe();
-      };
-    }
+        }
+      });
+    return () => {
+      unsubscribe();
+    };
   }, [noteId]);
 
   return [messages, setMessages];
